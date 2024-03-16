ee842bbc386b0794ae26a07f7ec7dcdf665fb555
Joselurc <143467266+Joselurc@users.noreply.github.com>
2024-03-15 19:52:32 -0500

    Update instituto_sise.sql
    Signed-off-by: Joselurc <143467266+Joselurc@users.noreply.github.com>

--- Clase1-main/Clase1-main/Registro y Edición/instituto_sise.sql
+++ Clase1-main/Clase1-main/Registro y Edición/instituto_sise.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 01-03-2024 a las 02:40:09
+-- Tiempo de generación: 13-03-2024 a las 03:04:17
 -- Versión del servidor: 8.0.17
 -- Versión de PHP: 7.3.10
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `instituto_sise`
+-- Base de datos: `login`
 --
 
 -- --------------------------------------------------------
@@ -29,23 +29,46 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `usuario` (
-  `id_user` int(11) NOT NULL,
-  `nombres` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
-  `apellidos` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
-  `dni` char(8) COLLATE utf8_unicode_ci NOT NULL,
-  `escuela` char(1) COLLATE utf8_unicode_ci NOT NULL,
-  `codigo` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
-  `categoria` char(1) COLLATE utf8_unicode_ci NOT NULL,
-  `pass` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `user` varchar(25) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `user` varchar(30) NOT NULL,
+  `pass` varchar(64) NOT NULL,
+  `estado` int(1) NOT NULL,
+  `id_grupo_usu` int(11) NOT NULL,
+  `user_activo` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_user`, `nombres`, `apellidos`, `dni`, `escuela`, `codigo`, `categoria`, `pass`, `user`) VALUES
-(1, 'Pedro Luis', 'Ramirez Saavedra', '74519677', '1', 'E0001', '1', '7e04da88cbb8cc933c7b89fbfe121cca', 'PRAMIREZ');
+INSERT INTO `usuario` (`id`, `nombre`, `user`, `pass`, `estado`, `id_grupo_usu`, `user_activo`) VALUES
+(15, 'David', 'DAVIDM', '7e04da88cbb8cc933c7b89fbfe121cca', 0, 5, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_grupo`
+--
+
+CREATE TABLE `usuario_grupo` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `usuario_grupo`
+--
+
+INSERT INTO `usuario_grupo` (`id`, `nombre`) VALUES
+(1, 'ADMINISTRADOR'),
+(2, 'PROCESOS'),
+(3, 'CAJERO'),
+(4, 'CONTADOR'),
+(5, 'ADMINISTRACION'),
+(6, 'TOMA DE MUESTRA'),
+(7, 'VENTAS'),
+(9, 'ECOGRAFIAS');
 
 --
 -- Índices para tablas volcadas
@@ -55,7 +78,13 @@ INSERT INTO `usuario` (`id_user`, `nombres`, `apellidos`, `dni`, `escuela`, `cod
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario_grupo`
+--
+ALTER TABLE `usuario_grupo`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -65,7 +94,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=203;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario_grupo`
+--
+ALTER TABLE `usuario_grupo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
